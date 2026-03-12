eec16bcf312fda24560e8f72db568d3bdf1d61f2
Gozel <gozel@MacBook-Air-Gozel.local>
2026-03-12 12:52:33 +0500

    header has been changed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <div class="header-top">
       <h1 class="app-title">My Tasks</h1>
-      <div class="date-badge" id="dateBadge">Пн, 9 марта</div>
+      <div class="date-badge">{{ dayOfWeek }}, {{ formattedDate }}</div>
     </div>
     <div class="stats-line">
       <div><span>{{ todoStore?.totalTodos }}</span> задач сегодня</div>
@@ -13,8 +13,22 @@
 
 <script setup>
 import { useTodoStore } from '../stores/useTodoStore';
+import { ref, onMounted, onUnmounted, computed } from 'vue';
+
 
 const todoStore = useTodoStore()
+const now = ref(new Date())
+
+const dayOfWeek = computed(() => {
+  return now.value.toLocaleDateString('ru-RU', { weekday: 'short' });
+});
+
+const formattedDate = computed(() => {
+  return now.value.toLocaleDateString('ru-RU', {
+    day: '2-digit',
+    month: 'long',
+  });
+});
 
 </script>
 
